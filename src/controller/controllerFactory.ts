861272ff1bd9837dfb16d89424096f697c1127ea
wenbo yang <wenbo.yang.84@gmail.com>
2024-05-17 20:09:10 -0700

    add integration tests

--- src/controller/controllerFactory.ts
+++
@@ -1,8 +0,0 @@
-import { Config } from '../config';
-import { ModelTrainingController } from './modelTrainingController';
-
-export class ControllerFactory {
-    public static makeModelTrainingController(config: Config): ModelTrainingController {
-        return new ModelTrainingController(config);
-    }
-}
